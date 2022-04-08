915fa22dcdb1042790bda11cf38e0314204bb7d6
Marcos Hernanz <marcosh.craft@gmail.com>
2022-04-08 16:27:07 +0200

    Did most of game logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,22 @@
 "use strict";
 
-import VALID_GUESSES from "src/js/validGuesses.js";
+import { VALID_INPUTS } from "./validInputs.js";
+import { VALID_GUESSES } from "./validGuesses.js";
+import { WORD_LIST } from "./wordList.js";
 
 const keyboardEl = document.getElementById("keyboard");
+const tableRowElements = document.querySelectorAll("tr");
 const tableCellElements = document.querySelectorAll("td");
+const errorMsgEl = document.getElementById("error-msg");
 
 let tableRow = 0;
 let tableCell = 0;
-let word = "";
+let wordGuess = "";
+let correctWord;
+
+const createCorrectWord = function () {
+  correctWord = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+};
 
 const enterLetter = function (letter) {
   let cell = tableCellElements[tableCell + tableRow * 5];
@@ -18,7 +27,7 @@ const enterLetter = function (letter) {
       return;
     }
 
-    submitWord(word);
+    submitWord(wordGuess);
     return;
   }
 
@@ -30,7 +39,7 @@ const enterLetter = function (letter) {
 
     cell.dataset.letter = "";
     cell.textContent = "";
-    word.slice(0, -1);
+    wordGuess = wordGuess.slice(0, -1);
     return;
   }
 
@@ -38,18 +47,60 @@ const enterLetter = function (letter) {
 
   cell.dataset.letter = letter;
   cell.textContent = letter;
-  word += letter;
+  wordGuess += letter;
   tableCell++;
 };
 
-const submitWord = function (word) {
-  if (!VALID_GUESSES.includes(word)) {
+const submitWord = function () {
+  console.log(wordGuess);
+
+  if (!VALID_GUESSES.includes(wordGuess)) {
     notValidWord();
     return;
   }
+
+  if (wordGuess === correctWord) {
+    winGame();
+  }
+
+  checkLetter();
+
+  tableRow++;
+  tableCell = 0;
+  wordGuess = "";
 };
 
-const notValidWord = function () {};
+const notValidWord = function () {
+  tableRowElements[tableRow].classList.add("not-valid-word");
+  tableRowElements[tableRow].addEventListener("animationend", function () {
+    tableRowElements[tableRow].classList.remove("not-valid-word");
+  });
+
+  errorMsgEl.classList.add("fade-out");
+  errorMsgEl.addEventListener("animationend", function () {
+    errorMsgEl.classList.remove("fade-out");
+  });
+};
+
+const winGame = function () {};
+
+const checkLetter = function () {
+  const wordGuessArr = wordGuess.split("");
+  const correctWordArr = correctWord.split("");
+  for (let i = 0; i < 5; i++) {
+    if (wordGuessArr[i] === correctWordArr[i]) {
+      tableCellElements[i + tableRow * 5].style.backgroundColor = "green";
+      tableCellElements[i + tableRow * 5].style.borderColor = "green";
+      correctWordArr[i] = "";
+    }
+  }
+  for (let i = 0; i < 5; i++)
+    if (correctWordArr.includes(wordGuessArr[i]) && wordGuessArr[i]) {
+      tableCellElements[i + tableRow * 5].style.backgroundColor = "yellow";
+      tableCellElements[i + tableRow * 5].style.borderColor = "yellow";
+      correctWordArr[correctWordArr.indexOf(wordGuessArr[i])] = "";
+    }
+};
 
 keyboardEl.addEventListener("click", function (e) {
   const keyEl = e.target.closest("li");
@@ -61,5 +112,13 @@ keyboardEl.addEventListener("click", function (e) {
 });
 
 document.addEventListener("keydown", function (e) {
+  if (!VALID_INPUTS.includes(e.key)) return;
+
   enterLetter(e.key);
 });
+
+const init = function () {
+  createCorrectWord();
+  console.log(correctWord);
+};
+init();
--- src/js/wordList.js
+++ src/js/wordList.js
@@ -1,4 +1,4 @@
-export const WORDS = [
+export const WORD_LIST = [
   "which",
   "there",
   "their",
