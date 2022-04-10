4328cc406d7dd33bde7ca3a0507ba752a1bdf081
Marcos Hernanz <marcosh.craft@gmail.com>
2022-04-10 12:54:23 +0200

    Implemented put letter animation

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,9 @@ import { WORD_LIST } from "./wordList.js";
 const keyboardEl = document.getElementById("keyboard");
 const tableRowElements = document.querySelectorAll("tr");
 const tableCellElements = document.querySelectorAll("td");
-const errorMsgEl = document.getElementById("error-msg");
+const messageEl = document.getElementById("message");
+
+const WIN_MESSAGES = ["Incredible!", "Genius!", "Amazing!"];
 
 let tableRow = 0;
 let tableCell = 0;
@@ -38,6 +40,8 @@ const enterLetter = function (letter) {
     tableCell--;
     cell = tableCellElements[tableCell + tableRow * 5];
 
+    cell.classList.remove("put-letter");
+
     cell.dataset.letter = "";
     cell.textContent = "";
     wordGuess = wordGuess.slice(0, -1);
@@ -45,6 +49,8 @@ const enterLetter = function (letter) {
   }
 
   if (tableCell >= 5) return;
+
+  cell.classList.add("put-letter");
 
   cell.dataset.letter = letter;
   cell.textContent = letter;
@@ -62,7 +68,14 @@ const submitWord = function () {
     winGame();
   }
 
-  checkLetter();
+  checkWord();
+
+  if (tableRow === 5) {
+    setTimeout(function () {
+      looseGame();
+      return;
+    }, 1000);
+  }
 
   tableRow++;
   tableCell = 0;
@@ -75,14 +88,39 @@ const notValidWord = function () {
     tableRowElements[tableRow].classList.remove("not-valid-word");
   });
 
-  errorMsgEl.classList.add("fade-out");
-  errorMsgEl.addEventListener("animationend", function () {
-    errorMsgEl.classList.remove("fade-out");
+  messageEl.textContent = "Not in word list";
+  messageEl.classList.add("fade-out");
+  messageEl.addEventListener("animationend", function () {
+    messageEl.classList.remove("fade-out");
   });
+};
+
+const showPlayAgain = function () {};
+
+const looseGame = function () {
+  document.removeEventListener("keydown", keyboardCallback);
+  keyboardEl.removeEventListener("click", screenKeyboardCallback);
+
+  messageEl.textContent = correctWord;
+  messageEl.style.opacity = 100;
+
+  showPlayAgain();
 };
 
 const winGame = function () {
   let animationDelay = 0;
+
+  document.removeEventListener("keydown", keyboardCallback);
+  keyboardEl.removeEventListener("click", screenKeyboardCallback);
+
+  setTimeout(function () {
+    messageEl.textContent =
+      WIN_MESSAGES[Math.floor(Math.random() * WIN_MESSAGES.length)];
+    messageEl.classList.add("fade-out");
+    messageEl.addEventListener("animationend", function () {
+      messageEl.classList.remove("fade-out");
+    });
+  }, 1000);
 
   for (let i = 0; i < 5; i++) {
     setTimeout(
@@ -107,9 +145,11 @@ const winGame = function () {
       animationDelay += 100;
     }
   }, 1000);
+
+  setTimeout(() => showPlayAgain(), 2500);
 };
 
-const checkLetter = function () {
+const checkWord = function () {
   const wordGuessArr = wordGuess.split("");
   const correctWordArr = correctWord.split("");
   const lettersInfo = {};
@@ -170,31 +210,34 @@ const checkLetter = function () {
 
   for (const [i, [_, value]] of Object.entries(Object.entries(lettersInfo))) {
     if (+i === 5) return;
-    setTimeout(
-      () =>
-        tableCellElements[+i + (tableRow - 1) * 5].classList.add(
-          `flip-${value}`
-        ),
-      flipDelay
-    );
+    setTimeout(() => {
+      tableCellElements[+i + (tableRow - 1) * 5].style.borderColor = "white";
+      tableCellElements[+i + (tableRow - 1) * 5].classList.remove("put-letter");
+
+      tableCellElements[+i + (tableRow - 1) * 5].classList.add(`flip-${value}`);
+    }, flipDelay);
     flipDelay += 100;
   }
 };
 
-keyboardEl.addEventListener("click", function (e) {
+const screenKeyboardCallback = function (e) {
   const keyEl = e.target.closest("li");
 
   if (!keyEl) return;
 
   const key = keyEl.dataset.key;
   enterLetter(key);
-});
+};
 
-document.addEventListener("keydown", function (e) {
+keyboardEl.addEventListener("click", screenKeyboardCallback);
+
+const keyboardCallback = function (e) {
   if (!VALID_INPUTS.includes(e.key)) return;
 
   enterLetter(e.key);
-});
+};
+
+document.addEventListener("keydown", keyboardCallback);
 
 const init = function () {
   createCorrectWord();
